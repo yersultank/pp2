b268bccd9670725364d0ea7c598e5e4db5584cd4
Yersultan <ersultank1010@gmail.com>
2025-01-23 00:33:57 +0600

    spaces for lab2

--- lab2/2.py
+++ lab2/2.py
@@ -2,51 +2,70 @@
 # there are several types of operators
 #Arithmetic Operators:
 print(10+2) # + Addition
+
 print(10-2) # - Subtraction
+
 print(10*2) # * Multiplication
+
 print(10/2) # / Divison
+
 print(10%2) # % Modulus
+
 print(10**2) # ** Exponentiation
+
 print(10//2) # // Floor division
 #Assignment Operators:
 x=5
 print(x)
+
 x=5
 x+=3
 print(x)
+
 x=5
 x-=3
 print(x)
+
 x=5
 x*=3
 print(x)
+
 x=5
 x/=3
 print(x)
+
 x=5
 x%=3
 print(x)
+
 x=5
 x**=3
 print(x)
+
 x=5
 x//=3
 print(x)
+
 x=5
 x&=3
 print(x)
+
 x=5
 x|=3
 print(x)
+
 x=5
 x^=3
 print(x)
+
 x=5
 x>>=3
 print(x)
+
 x=5
 x<<=3
 print(x)
+
 x=5
 print(x:=3)
 
@@ -54,16 +73,23 @@ print(x:=3)
 x=3
 y=2
 print(x==y)
+
 print(x!=y)
+
 print(x>y)
+
 print(x<y)
+
 print(x>=y)
+
 print(x<=y)
 
 #Logical Operators:
 x=3
 print(x>0 and x<5)
+
 print(x>10 or x<6)
+
 print(not(x>0 and x<5))
 
 #Identity Operators:
@@ -71,33 +97,53 @@ x=["wow", "that's crazy"]
 y=["wow", "that's crazy"]
 z=x
 print(x is z)
+
 print(x is y)
+
 print(x==y)
 
 #Membership Operators:
 x=["wow", "that's crazy"]
 print("wow" in x)
+
 print("hello" not in x)
 
 #Bitwise Operators:
 print(6&3)
+
 print(6|3)
+
 print(6^3)
+
 print(~3)
+
 print(3<<2)
+
 print(8>>2)
 
 #Operator Precedence
 print((6+3)-(6+3)) # Parentheses
+
 print(100+ 5*3) # Multiplication, division, floor division, and modulus
+
 print(100- 3**3) # Exponentiation
+
 print(100+ ~3) # Unary plus, unary minus, and bitwise NOT
+
 print(100 - 5*3) # Addition and subtraction
+
 print(8 >> 4-2) # Bitwise left and right shifts
+
 print(6 & 2+1) # Bitwise AND
+
 print(6 ^ 2+1) # Bitwise XOR
+
 print(6 | 2+1) # Bitwise OR
+
 print(5 == 4+1) # Comparisons, identity, and membership operators
+
 print(not 5 == 5) # Logical NOT
+
 print(2 or 3 and 4) #AND
+
 print(4 or 5+10 or 8) #OR
--- lab2/3.py
+++ lab2/3.py
@@ -2,21 +2,29 @@
 # List is a collection which is ordered and changeable. Allows duplicate members.
 mylist=["mouse", "monitor", "keybord"]
 print(mylist)
+
 # there might be duplicates in list
 mylist=["mouse", "monitor", "mouse" ,"keybord"]
 print(mylist)
+
 #len() function:
 mylist=["mouse", "monitor", "keybord"]
 print(len(mylist))
+
 #there can be any data type in list:
 mylist1=["mouse", "monitor", "keybord"]
 print((mylist1))
+
 mylist2=[1,2,3,4]
 print((mylist2))
+
 mylist3=[True, True, False]
 print((mylist3))
+
 list1=[1, "hi", True, 1.05]
 print(list1)
+
 print(type(list1))
+
 newlist=list(("mouse", "monitor", "keybord"))
 print(newlist)
