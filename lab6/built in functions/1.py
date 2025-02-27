e66e444d84c49a82828040ab4cdfa19726e88944
Yersultan <ersultank1010@gmail.com>
2025-02-27 18:47:13 +0600

    lab6

---
+++ lab6/built in functions/1.py
@@ -0,0 +1,48 @@
+import math
+import time
+#1 task
+# size=int(input("size of list:"))
+# list=[]
+# for i in range(1,size+1):
+#     element=int(input("enter number:"))
+#     list.append(element)
+# prod=eval('*'.join(map(str,list)))
+# print(prod)
+
+#2 task
+# s=input("type a string:")
+# cnt_upper=sum(1 for i in s if i.isupper())
+# cnt_lower=sum(1 for i in s if i.islower())
+# print(f"There are {cnt_upper} uppercase letters and {cnt_lower} lowercase letters")
+
+#3 task
+# def is_palinfrome(s):
+#     return s==s[::-1]
+# word=input("type a word:")
+# print(is_palinfrome(word))
+        
+#4 task
+# num=int(input("write a number:"))
+# mlsec=int(input("choose milliseconds:"))
+# num=math.sqrt(num)
+# time.sleep(mlsec/1000)
+# print(num)
+
+#5 task
+size=int(input("size of tuple:"))
+list=[]
+for i in range(1,size+1):
+    element=input("enter element:")
+    if element==0:
+        list.append(0)
+    elif element=="False":
+        list.append(False)
+    elif element=="True":
+        list.append(True)
+    elif element=="1":
+        list.append(1)
+    else:
+        list.append(element)
+tuple=tuple(list)
+x=all(tuple)
+print(x)
---
+++ lab6/dir- and -files/1.py
@@ -0,0 +1,103 @@
+import os
+# 1 task
+# path=r"C:\Users\Ерс\Desktop"
+# all=list(os.listdir(path))
+# files=[]
+# dir=[]
+# for i in all:
+#     element_path=os.path.join(path,i)
+#     if os.path.isfile(element_path):
+#         files.append(i)
+#     if os.path.isdir(element_path):
+#         dir.append(i)
+# print(f"all directories and files: {all}")
+# print("********************")
+# print(f"only files: {files}")
+# print("********************")
+# print(f"only directories: {dir}")
+
+#2 task
+# path=r"C:\Users\Ерс\Downloads"
+# if os.path.exists(path):
+#     print("file exists")
+#     if os.access(path,os.R_OK):
+#         print("File is readble")
+#     else:
+#         print("file is not readble")
+    
+#     if os.access(path, os.W_OK):
+#         print("file is writable")
+#     else:
+#         print("file is not writable")
+    
+#     if os.access(path, os.X_OK):
+#         print("file is executable")
+#     else:
+#         print("file is not executable")
+# else:
+#     print("file does not exist")
+
+
+#3 task
+# path=r"C:\Users\Ерс\Downloads"
+# if os.path.exists(path):
+#     print("file exists")
+#     print(f"name of the file {os.path.basename(path)}")
+#     print(f"name of the directory {os.path.dirname(path)}")
+# else:
+#     print("file does not exist")
+
+
+#4 task
+# path=r"C:\Users\Ерс\Desktop\w3\abcd.txt"
+# f=open(path,'r')
+# cnt=0
+# for i in f:
+#     cnt+=1
+# print(cnt)
+
+
+#5 task
+# path=r"C:\Users\Ерс\Desktop\w3\abcd.txt"
+# list=["artur morgan", " john marston", " mika the rat"]
+# f=open(path, 'w')
+# for i in list:
+#     f.write(i)
+# print(f.read)
+
+#6 task
+# for i in range(0,26):
+#     s=chr(65+i)+".txt"
+#     f=open(s,'w')
+#     f.write("")
+#     f.close()
+
+
+#7 task
+# path=r"C:\Users\Ерс\Desktop\w3\abcd.txt"
+# f=open(path, 'r')
+# f1=open('efg.txt','w')
+# for i in f:
+#     f1.write(i)
+# f.close()
+# f1.close()
+# with open('efg.txt','r') as f1:
+#     print(f1.read())
+
+
+#8 task
+# path=r"C:\Users\Ерс\Desktop\w3\ab.txt"
+# if os.path.exists(path):
+#     print("this file exists and will be deleted")
+#     os.remove(path)
+# else:
+#     print("this file does not exist")
+
+# for i in range(0,26):
+#     s=chr(65+i)+".txt"
+#     if os.path.exists(s):
+#         print("this file exists and will be deleted")
+#         os.remove(s)
+#     else:
+#         print("this file does not exist")
+# code above to delete all files A.txt, B.txt,..., Z.txt
